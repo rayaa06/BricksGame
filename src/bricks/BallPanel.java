d5654233ccd0e20d7a615383f8441e6d46180151
Magi <magdalenanikolova13@gmail.com>
2024-01-10 21:55:00 +0200

    mvuikbj

--- src/bricks/BallPanel.java
+++ src/bricks/BallPanel.java
@@ -1,76 +1,47 @@
+package bricks;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.ImageObserver;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class BallPanel extends JPanel {
-    private final Image img;
-    private int xCoord;
-    private int yCoord;
-    private Dimension preferredSize;
+public class BallPanel extends JFrame implements ActionListener {
+    private JButton leftBtn = new JButton("Left");
+    private JButton rightBtn = new JButton("Right");
+    private JButton upBtn = new JButton("Up");
+    private JButton downBtn = new JButton("Down");
+    private MovingBall ballPanel;
 
-    private boolean dirXRight = true;
-    private boolean dirYDown = true;
-    BallPanel(){
-        this.img = new ImageIcon("resources/Red-Ball-PNG.png").getImage();
-        this.xCoord = -1;
-        this.yCoord = -1;
-        this.preferredSize = new Dimension(300,200);
-    }
+    public BallPanel(){
+        ballPanel = new MovingBall();
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(leftBtn);
+        buttonPanel.add(rightBtn);
+        buttonPanel.add(upBtn);
+        buttonPanel.add(downBtn);
 
-    public void left() {
-        xCoord -= 5;
-        repaint();
+        ballPanel.setBackground(Color.WHITE);
+        this.add(ballPanel);
+        this.add(buttonPanel, BorderLayout.SOUTH);
+        leftBtn.addActionListener(this);
+        rightBtn.addActionListener(this);
+        upBtn.addActionListener(this);
+        downBtn.addActionListener(this);
     }
-    public void right() {
-        xCoord += 5;
-        repaint();
-    }
-    public void up() {
-        yCoord -= 5;
-        repaint();
-    }
-    public void down() {
-        yCoord += 5;
-        repaint();
-    }
-
     @Override
-    public Dimension getPreferredSize() {
-        return preferredSize;
+    public void actionPerformed(ActionEvent buttonPressed) {
+        if(buttonPressed.getSource() == leftBtn)
+            ballPanel.left();
+        else if(buttonPressed.getSource() == rightBtn)
+            ballPanel.right();
+        else if(buttonPressed.getSource() == upBtn)
+            ballPanel.up();
+        else if(buttonPressed.getSource() == downBtn)
+            ballPanel.down();
+    }
+    
+    public MovingBall getBallPanel() {
+        return ballPanel;
     }
 
-    @Override
-    protected void paintComponent(Graphics graphics) {
-        super.paintComponent(graphics);
-
-        graphics.drawImage(img, xCoord, yCoord, this);
-    }
-
-    public void move() throws InterruptedException {
-
-        int width = 400;
-        int height = 400;
-
-        while(true) {
-
-            if((dirXRight && xCoord+125>width) || (!dirXRight && xCoord-125<0))
-                dirXRight=!dirXRight;
-
-            if((dirYDown && yCoord+125>height) || (!dirYDown && yCoord-125<0))
-                dirYDown=!dirYDown;
-
-
-            if(dirXRight)
-                right();
-            else
-                left();
-
-            if(dirYDown)
-                down();
-            else
-                up();
-
-            Thread.sleep(50);
-        }
-    }
 }
